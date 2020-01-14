805a38ef0a5fe629a2d8895afc82d737576c00a7
juanIgnacioEchaide <jie_chaide@hotmail.com>
2020-01-14 16:35:10 -0300

    finally UI connected but FeedBack

--- src/UI/OptionPanel.jsx
+++ src/UI/OptionPanel.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
-import { View, Text, Button, Image, TextInput, ScrollView, FlatList, Modal,TouchableOpacity } from 'react-native';
+import { View, Text, Button, Image, TextInput, ScrollView, FlatList, Modal, TouchableOpacity } from 'react-native';
 
 const OptionPanel = props => {
 
-/*     const[{a='A',b='C',c='C',d='D'}]=useState(); */
+    const [a = 'A', b = 'B', c = 'C', d = 'D'] = useState();
 
-    
-    
+
+
     return (
         <View style={{
             borderColor: 'steelblue'
         }}>
             <View style={{ flexDirection: 'row' }}>
-                
-                <TouchableOpacity style={{height:1}}>
-                    <Text>asds</Text>
+
+                <TouchableOpacity style={{ height: 1 }}>
+                    <Text>{a}</Text>
                 </TouchableOpacity>
                 <TouchableOpacity>
-                <Text>skfh pias</Text>
+                    <Text>{b}</Text>
                 </TouchableOpacity>
             </View>
             <View style={{ flexDirection: 'row' }}>
                 <TouchableOpacity>
-                <Text>j woueg</Text>
+                    <Text>{c}</Text>
                 </TouchableOpacity>
                 <TouchableOpacity>
-                <Text>sbcbahlsdb</Text>
+                    <Text>{d}</Text>
                 </TouchableOpacity>
             </View>
         </View>
